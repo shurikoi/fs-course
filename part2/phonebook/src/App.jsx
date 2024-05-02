62a09c6ed7d4a838a3ea0bfbea1f093e21af1bbc
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2024-05-02 23:33:46 +0200

    task 2.11 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,32 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import PersonForm from "./PersonForm"
 import Persons from "./Persons"
+import axios from "axios"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "098" },
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
+
+  useEffect(() => {
+    axios("http://localhost:3001/persons").then((response) =>
+      setPersons(response.data)
+    )
+  }, [])
 
   const handleAddNote = (event) => {
     event.preventDefault()
     const isUnique = persons.some((person) => person.name == newName)
+    const lastDbId = persons.slice(-1)[0]?.id
 
     if (isUnique) return alert(`${newName} is already added to phonebook`)
+
+    axios
+      .post("http://localhost:3001/persons", {
+        name: newName,
+        number: newNumber,
+        id: lastDbId + 1,
+      })
 
     setPersons([...persons, { name: newName, number: newNumber }])
     setNewName("")
