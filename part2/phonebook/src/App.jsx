fc1a25315597e3417f3203506b12c99215d04949
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2024-05-02 22:04:26 +0200

    task 2.7 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react"
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }, { name: "Artoe Hellas" }])
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas" },
+    { name: "Artoe Hellas" },
+  ])
   const [newName, setNewName] = useState("")
-  console.log(newName)
 
   const handleAddNote = (event) => {
     event.preventDefault()
+    const isUnique = persons.some(person => person.name == newName)
+
+    if (isUnique)
+      return alert(`${newName} is already added to phonebook`)
+
     setPersons([...persons, { name: newName }])
     setNewName("")
   }
@@ -16,14 +23,20 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleAddNote}>
         <div>
-          name: <input value={newName} onChange={() => setNewName(event.target.value)} />
+          name:{" "}
+          <input
+            value={newName}
+            onChange={() => setNewName(event.target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   )
 }
