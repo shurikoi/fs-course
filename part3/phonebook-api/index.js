e36340821b0e5e14cfdb9a7ee7515abf7686f9c1
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2024-05-03 14:16:48 +0200

    task 3.2 done

--- part3/phonebook-api/index.js
+++ part3/phonebook-api/index.js
@@ -1,36 +1,38 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const PORT = 3001
 
-let notes = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+  )
 })
 
-app.get('/api/persons', (request, response) => {
-  response.json(notes)
+app.get("/api/persons", (request, response) => {
+  response.json(persons)
 })
 
 app.listen(PORT, () => {
