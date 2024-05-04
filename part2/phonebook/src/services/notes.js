5495248836dd41640b7392e9acba32015e491b47
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2024-05-04 23:08:08 +0200

    task 3.9 done

--- part2/phonebook/src/services/notes.js
+++ part2/phonebook/src/services/notes.js
@@ -1,9 +1,9 @@
 import axios from "axios"
-const baseUrl = "http://localhost:3001/persons/"
+const baseUrl = "http://localhost:3001/api/persons/"
 
 export const getAllNotes = async () => await axios(baseUrl)
 
 export const createNote = async (note) => await axios.post(baseUrl, note)
 
 export const deleteNote = async (id) =>
-  await axios.delete(`http://localhost:3001/persons/${id}`)
+  await axios.delete(`${baseUrl}${id}`)
--- part3/phonebook-api/index.js
+++ part3/phonebook-api/index.js
@@ -2,7 +2,9 @@ const express = require("express")
 const app = express()
 const PORT = 3001
 const morgan = require("morgan")
+const cors = require("cors")
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan("tiny"))
 
