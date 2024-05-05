7e4456919073a9e2d165d01de96729318e9adc6e
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2024-05-05 09:30:18 +0200

    task 3.12 done

---
+++ part3/phonebook-api/mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3) {
+  console.log("give password as argument")
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://bezverkhniispace:${password}@phonebook.56y9sxw.mongodb.net/?retryWrites=true&w=majority&appName=phonebook`
+
+mongoose.set("strictQuery", false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model("Person", personSchema)
+
+if (!process.argv[3]) {
+  Person.find({}).then((result) => {
+    result.forEach((note) => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+  return 
+}
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+})
+
+person.save().then((result) => {
+  console.log("person saved!")
+  mongoose.connection.close()
+})
