7add163bacec908c91226914dd61cfd7a264e716
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2024-05-06 12:41:19 +0200

    task 3.22 done

--- part3/phonebook-api/index.js
+++ part3/phonebook-api/index.js
@@ -1,17 +1,17 @@
-require("dotenv").config()
-const express = require("express")
+require('dotenv').config()
+const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001
-const morgan = require("morgan")
-const cors = require("cors")
-const Person = require("./models/person")
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
 
-app.use(express.static("dist"))
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
-app.use(morgan("tiny"))
+app.use(morgan('tiny'))
 
-app.get("/info", (req, res) => {
+app.get('/info', (req, res) => {
   Person.countDocuments({}).then((length) =>
     res.send(
       `<p>Phonebook has info for ${length} people</p><p>${new Date()}</p>`
@@ -19,28 +19,28 @@ app.get("/info", (req, res) => {
   )
 })
 
-app.get("/api/persons", (req, res) => {
+app.get('/api/persons', (req, res) => {
   Person.find({}).then((persons) => res.json(persons))
 })
 
-app.get("/api/persons/:id", (req, res, next) => {
+app.get('/api/persons/:id', (req, res, next) => {
   const id = req.params.id
   Person.findOne({ _id: id })
     .then((person) => {
       if (person) res.json(person)
-      else response.status(404).end()
+      else res.status(404).end()
     })
     .catch((error) => next(error))
 })
 
-app.post("/api/persons/", (req, res) => {
+app.post('/api/persons/', (req, res) => {
   const { name, number } = req.body
   // const findName = await Person.findOne({ name })
   // let isNameExists = !!findName
 
   if (!(name && number))
     res.status(400).json({
-      error: "some field(s) are missing",
+      error: 'some field(s) are missing',
     })
 
   // if (isNameExists)
@@ -56,19 +56,20 @@ app.post("/api/persons/", (req, res) => {
   ]).then((person) => res.json(person[0]))
 })
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete('/api/persons/:id', (req, res) => {
   const id = req.params.id
-  Person.findByIdAndDelete(id).then((result) => {
-    res.status(204).end()
-  })
-  .catch((error) => next(error))
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch((error, next) => next(error))
 })
 
 const errorHandler = (error, req, res, next) => {
   console.error(error.message)
 
-  if (error.name === "CastError") {
-    return res.status(400).send({ error: "malformatted id" })
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
   }
 
   next(error)
--- part3/phonebook-api/models/person.js
+++ part3/phonebook-api/models/person.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false)
+mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
-console.log("Connecting to...", url)
+console.log('Connecting to...', url)
 
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB")
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message)
+    console.log('error connecting to MongoDB:', error.message)
   })
 
 const personSchema = new mongoose.Schema({
@@ -20,7 +20,7 @@ const personSchema = new mongoose.Schema({
   number: String,
 })
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
@@ -28,4 +28,4 @@ personSchema.set("toJSON", {
   },
 })
 
-module.exports = mongoose.model("Person", personSchema)
+module.exports = mongoose.model('Person', personSchema)
