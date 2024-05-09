ba7cacced8df38dfac054701cd5bdc3255344554
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2024-05-09 09:50:03 +0200

    task 4.10

--- part4/bloglist-api/tests/blog_api.test.js
+++ part4/bloglist-api/tests/blog_api.test.js
@@ -1,25 +1,52 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
+const Blog = require('../models/blog')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const assert = require('assert')
+const { initialBlogs } = require('./test_helper')
 
 const api = supertest(app)
 
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  console.log("DB cleared")
+  await Blog.insertMany(initialBlogs)
+  console.log("Saved")
+})
+
 test('there are two blogs in the db', async () => {
-  const response = await api
+  const { body } = await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 
-  assert.strictEqual(response.body.length, 2)
+  assert.strictEqual(body.length, initialBlogs.length)
 })
 
-test('is unique identifier property of the blog posts is named id', async () => {
-  const response = await api.get('/api/blogs')
-  const isWithId = response.body.every((blog) => blog.hasOwnProperty('id'))
+test('unique identifier property of the blog posts is named id', async () => {
+  const { body } = await api.get('/api/blogs')
+  const isWithId = body.every((blog) => blog.hasOwnProperty('id'))
 
   assert.strictEqual(isWithId, true)
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'TEST',
+    author: 'TEST',
+    url: 'TEST',
+    likes: 2,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const allBlogs = await api.get('/api/blogs')
+  assert.strictEqual(allBlogs.body.length, initialBlogs.length + 1)
 })
 
 after(async () => {
---
+++ part4/bloglist-api/tests/test_helper.js
@@ -0,0 +1,16 @@
+const initialBlogs = [
+  {
+    title: 'Hi',
+    author: 'Oleksandr',
+    url: 'TEST',
+    likes: 1,
+  },
+  {
+    title: 'There',
+    author: 'Antonii',
+    url: 'TEST',
+    likes: 2,
+  },
+]
+
+module.exports = {initialBlogs}
