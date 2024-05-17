9a114307d03cd6df9ef47d2129e74af58a8be737
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2024-05-17 14:12:31 +0200

    task 4.19 done

--- part4/bloglist-api/controllers/blogs.js
+++ part4/bloglist-api/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -11,8 +20,10 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const { title, author, url, likes } = request.body
 
-  const usersInDb = await User.find({})
-  const user = usersInDb[0]
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) response.status(401).json({ error: 'token invalid' })
+    
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({ title, author, url, likes, user })
 
